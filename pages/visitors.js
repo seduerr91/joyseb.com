62436a2b461c46c6f769bcaf9469a33095b18b9a
Sebastian <Duerr.sebastian@gmail.com>
2023-02-03 17:32:09 -0800

    update parking directions

--- pages/visitors.js
+++ pages/visitors.js
@@ -17,10 +17,12 @@ function Visitors() {
             <Text>Welcome to our home at <Link href='https://goo.gl/maps/wt8GhQy3Nqg5LQZt9'>Apartment F102, 34626 SE Swenson Dr, Snoqualmie, WA 98065</Link>.</Text>
 
 
-            <Text>Parking is possible right in front of our house or pretty good following SE Swenson Drive across the bridge.</Text>
-            <Text>Our I-90 highway exit is getting really busy both ways around rush hour (5:00pm - 7:30pm). Hence, we recommend the highway exit at mile 27 and then to backtrack.</Text>
-
+            <Text>Unfortunately, parking is not too easy right inside the Woodlands.</Text>
+            <Text>There is two options: straight down on the right side, and the "future resident" parkings are public.</Text>
+            <Text>Better parking is leaving Woodlands to the left and following SE Swenson Drive across the bridge for some 200 feet. Then, there is plenty of street parking available.</Text>
             
+            <Text>Generally, the I-90 highway exit 25 (Snoqualmie) is getting really busy both ways around rush hour (5:00pm - 7:30pm). Hence, we recommend the highway exit at mile 27 and then to backtrack.</Text>
+
             <Text>Join our Wifi Network by scanning this QR code:</Text>
             <Image height='10rem' width='10rem' src='./img/qr.png'/>
             <Text>Upon arrival, just ring our bell.</Text>
